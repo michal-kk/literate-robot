2658d6be5b646c0c78e4187ad4d0c76a84e1b966
Michal Kopec <michal.kopec@ericsson.com>
2017-10-24 10:48:01 +0200

    Create test

--- cloud-stream-message-producer/src/test/java/com/example/demo/CloudStreamMessageProducerApplicationTests.java
+++ cloud-stream-message-producer/src/test/java/com/example/demo/CloudStreamMessageProducerApplicationTests.java
@@ -1,16 +1,44 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.messaging.Message;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CloudStreamMessageProducerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private TestRestTemplate restTemplate;
 
+  @Autowired
+  private MessageCollector messageCollector;
+
+  @Autowired
+  private Source source;
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testGetRequest() {
+    String responseBody = restTemplate.getForObject("/", String.class);
+    assertThat(responseBody).isEqualTo("ok");
+
+    Message<String> message = (Message<String>) messageCollector.forChannel(source.output()).poll();
+    assertThat(message.getPayload()).contains("{\"number\":1");
+
+    responseBody = restTemplate.getForObject("/", String.class);
+    assertThat(responseBody).isEqualTo("ok");
+
+    message = (Message<String>) messageCollector.forChannel(source.output()).poll();
+    assertThat(message.getPayload()).contains("{\"number\":2");
+  }
 }
